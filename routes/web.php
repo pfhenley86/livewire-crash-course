beba68463665a9dabe102c07ada564338295976b
Ford Henley <info@fordswebhelp.com>
2024-03-31 15:55:57 -0500

    Started Create Properties

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Livewire\Tasks\TaskCreate;
+use App\Livewire\Tasks\TaskIndex;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('tasks', TaskIndex::class);
+Route::get('/tasks/create', TaskCreate::class);
